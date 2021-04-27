5aa9f55c7424e67367e0e3bcd71ca8f1c0d84c8b
NitramMann21 <martin.mann@onlinehome.de>
2021-04-27 15:40:34 +0200

    ?help now shows the real prefix

--- src/de/onlinehome/mann/martin/exenbot/commands/HelpCommand.java
+++ src/de/onlinehome/mann/martin/exenbot/commands/HelpCommand.java
@@ -2,6 +2,7 @@ package de.onlinehome.mann.martin.exenbot.commands;
 
 import java.util.concurrent.TimeUnit;
 
+import de.onlinehome.mann.martin.exenbot.YamlUtil;
 import de.onlinehome.mann.martin.exenbot.commands.types.ServerCommand;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
@@ -24,15 +25,17 @@ public class HelpCommand implements ServerCommand {
 
 	@Override
 	public void performCommand(Member m, TextChannel channel, Message message) {
+		String prefix = YamlUtil.getPrefix(channel.getGuild());
+		
 		EmbedBuilder builder = new EmbedBuilder().setTitle("Hilfe").setColor(0x2efe2e);
 		builder.setDescription("**Commands für jeden**");
-		builder.addField(".help", "Zeigt diese Liste an.", false);
-		builder.addField(".temptalk <create|delete|size> [Name (*create*) bzw. Größe (*size*)]",
+		builder.addField(prefix + "help", "Zeigt diese Liste an.", false);
+		builder.addField(prefix + "temptalk <create|delete|size> [Name (*create*) bzw. Größe (*size*)]",
 				"Erstellt, löscht oder setzt die Größe eines Temptalks.", false);
 		if (m.hasPermission(Permission.MANAGE_SERVER)) {
 			builder.addField("", "**Admincommands**", false);
-			builder.addField(".settings", "Einstellungen.", false);
-			builder.addField(".stop [maintenance|restart]", "Stoppt den Bot.", false);
+			builder.addField(prefix + "settings", "Einstellungen.", false);
+			builder.addField(prefix + "stop [maintenance|restart]", "Stoppt den Bot.", false);
 		}
 		channel.sendMessage(builder.build()).complete().delete().queueAfter(30, TimeUnit.SECONDS);
 		message.delete().queue();
